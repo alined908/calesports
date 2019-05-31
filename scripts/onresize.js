18ccf5d04bf7d91ee29b9d47276a0753c11641d0
Daniel Lee <daniellee0228@berkeley.edu>
2019-05-30 18:02:40 -0700

    make website load faster

--- scripts/onresize.js
+++ scripts/onresize.js
@@ -1,13 +1,23 @@
+var doit;
+function resizedend(){
+  document.getElementById("side-nav").style.display = "block"
+}
+
 window.onresize  = () =>{
     if (window.innerWidth > 1100){
+        console.log("bye");
         document.getElementById("side-nav").style.width = "100%";
         document.getElementsByTagName("main")[0].style.opacity = 1;
         document.getElementsByTagName("footer")[0].style.opacity = 1;
-    }
-    else if (window.innerWidth < 1000){
-        document.getElementById("side-nav").style.display = "none";
-
-        document.getElementById("side-nav").style.width = "0";
         document.getElementById("side-nav").style.display = "block";
+    }
+    if (window.innerWidth <= 1100){
+        console.log("hello");
+        document.getElementById("side-nav").style.display = "none";
+        document.getElementById("side-nav").style.width = "0";
+        clearTimeout()
+        doit = setTimeout(function(){
+          resizedend();
+        }, 10);
     }
 }
